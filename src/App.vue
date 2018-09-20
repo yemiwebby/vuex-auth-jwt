1d402672518fd871aedbdc3a534f8e02eb2a5e09
yemiwebby <yemiwebby@gmail.com>
2018-09-20 05:18:43 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,44 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+      <span v-else> | <a @click="logout">Login</a></span>
     </div>
     <router-view/>
   </div>
 </template>
 
+
+<script>
+export default {
+  created: function() {
+    this.$http.interceptors.response.use(undefined, function(err) {
+      return new Promise(function(resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch(logout);
+        }
+        throw err;
+      });
+    });
+  },
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+  methods: {
+    logout: function() {
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/login");
+      });
+    }
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
